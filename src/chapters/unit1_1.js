9667b4de37f25a990eea4d7f08f422d559daeb80
marquesadilla <marquesbatoon@gmail.com>
2023-09-07 11:48:26 -1000

    added some words

--- src/chapters/unit1_1.js
+++ src/chapters/unit1_1.js
@@ -6,7 +6,9 @@ const unit1_1 = {
         配属: { 読み方: ['はいぞく'], form: '名詞、する動詞、他動詞', 意味: "assignment (of a person)" },
         配属先: { 読み方: ['はいぞくさき'], form: '名詞', 意味: "an office to which one was assigned" },
         挨拶: { 読み方: ['あいさつ'], form: '名詞、する動詞', 意味: "to greet" },
-
+        海外事業部: { 読み方: ['かいがいじぎょうぶ'], form: '名詞', 意味: "overseas department" },
+        ことになる: { 読み方: ['V〜ことになる'], form: '表現', 意味: "it is decided that" },
+        一言: { 読み方: ['ひとこと'], form: '名詞、する動詞', 意味: "a brief word (to say)" },
         //: { 読み方: [''], form: '', 意味: "" },
     },
 
