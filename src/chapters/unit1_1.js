c68f4cf58f5f96811d9cbbec64291db07036f23b
marquesadilla <marquesbatoon@gmail.com>
2023-09-08 09:23:42 -1000

    added tango

--- src/chapters/unit1_1.js
+++ src/chapters/unit1_1.js
@@ -21,8 +21,22 @@ const unit1_1 = {
     },
 
     yomerebaii: {
-
-
+        "sales department": { 読み方: ['営業部'], form: '名詞', 意味: "えいぎょうぶ" },
+        "personnel department": { 読み方: ['人事部'], form: '名詞', 意味: "じんじぶ" },
+        "general affair department": { 読み方: ['総務部'], form: '名詞', 意味: "そうむぶ" },
+        "public relations department": { 読み方: ['広報部'], form: '名詞', 意味: "こうほうぶ" },
+        "advertising department": { 読み方: ['宣伝部'], form: '名詞', 意味: "せんでんぶ" },
+        "marketing department": { 読み方: ['マーケティング部'], form: '名詞', 意味: "マーケティングぶ" },
+        "finance department": { 読み方: ['財務部'], form: '名詞', 意味: "ざいむぶ" },
+        "chairperson": { 読み方: ['会長'], form: '名詞', 意味: "かいちょう" },
+        "president": { 読み方: ['社長'], form: '名詞', 意味: "しゃちょう" },
+        "vice president": { 読み方: ['副社長'], form: '名詞', 意味: "副社長" },
+        "senior managing director": { 読み方: ['専務'], form: '名詞', 意味: "せんむ" },
+        "managing director": { 読み方: ['常務'], form: '名詞', 意味: "じょうむ" },
+        "general manager": { 読み方: ['部長'], form: '名詞', 意味: "ぶちょう" },
+        "section manager": { 読み方: ['課長'], form: '名詞', 意味: "かちょう" },
+        "chief": { 読み方: ['係長'], form: '名詞', 意味: "かかりちょう" },
+        "supervisor": { 読み方: ['主任'], form: '名詞', 意味: "しゅにん" },
         //: { 読み方: [''], form: '', 意味: "" },
 
     },
--- src/kanji.js
+++ src/kanji.js
@@ -1,11 +1,19 @@
 const kanji = {
     tango: {
-        // Unit 1_1
-        
+        // Unit L_1
+        株式会社: { 読み方: ['かぶしきがいしゃ'], form: '名詞', 意味: "Corporation" },
+        就職: { 読み方: ['（〜に）しゅうしょく'], form: '名詞、する動詞', 意味: "to find a job (at~), finding employment" },
+        社内: { 読み方: ['しゃない'], form: '名詞', 意味: "within the company" },
+        配属: { 読み方: ['はいぞく'], form: '名詞、する動詞、他動詞', 意味: "assignment (of a person)" },
+        配属先: { 読み方: ['はいぞくさき'], form: '名詞', 意味: "an office to which one was assigned" },
+        挨拶: { 読み方: ['あいさつ'], form: '名詞、する動詞', 意味: "to greet" },
+        海外事業部: { 読み方: ['かいがいじぎょうぶ'], form: '名詞', 意味: "overseas department" },
+        ことになる: { 読み方: ['V〜ことになる'], form: '表現', 意味: "it is decided that" },
+        一言: { 読み方: ['ひとこと'], form: '名詞、する動詞', 意味: "a brief word (to say)" },
         //: { 読み方: [''], form: '', 意味: "" },
     },
     kaku: {
-        // Unit 1_1
+        // Unit L_1
         
         // : { 読み方: [''], form: '', 意味: "" },
 
@@ -13,8 +21,23 @@ const kanji = {
     },
 
     yomerebaii: {
-        // Unit 1_1
-
+        // Unit L_1
+        "sales department": { 読み方: ['営業部'], form: '名詞', 意味: "えいぎょうぶ" },
+        "personnel department": { 読み方: ['人事部'], form: '名詞', 意味: "じんじぶ" },
+        "general affair department": { 読み方: ['総務部'], form: '名詞', 意味: "そうむぶ" },
+        "public relations department": { 読み方: ['広報部'], form: '名詞', 意味: "こうほうぶ" },
+        "advertising department": { 読み方: ['宣伝部'], form: '名詞', 意味: "せんでんぶ" },
+        "marketing department": { 読み方: ['マーケティング部'], form: '名詞', 意味: "マーケティングぶ" },
+        "finance department": { 読み方: ['財務部'], form: '名詞', 意味: "ざいむぶ" },
+        "chairperson": { 読み方: ['会長'], form: '名詞', 意味: "かいちょう" },
+        "president": { 読み方: ['社長'], form: '名詞', 意味: "しゃちょう" },
+        "vice president": { 読み方: ['副社長'], form: '名詞', 意味: "副社長" },
+        "senior managing director": { 読み方: ['専務'], form: '名詞', 意味: "せんむ" },
+        "managing director": { 読み方: ['常務'], form: '名詞', 意味: "じょうむ" },
+        "general manager": { 読み方: ['部長'], form: '名詞', 意味: "ぶちょう" },
+        "section manager": { 読み方: ['課長'], form: '名詞', 意味: "かちょう" },
+        "chief": { 読み方: ['係長'], form: '名詞', 意味: "かかりちょう" },
+        "supervisor": { 読み方: ['主任'], form: '名詞', 意味: "しゅにん" },
         // : { 読み方: [''], form: '', 意味: ""  },
     }
 }
