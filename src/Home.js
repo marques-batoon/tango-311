52ce1b0294b828b7057f8bbd0b74ef0cc8794790
marquesadilla <marquesbatoon@gmail.com>
2023-11-13 10:12:53 -1000

    added lesson 4

--- src/Home.js
+++ src/Home.js
@@ -56,7 +56,7 @@ class Home extends React.Component {
 
                 <div className="homeButtonContainer">
                     <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="L4-tango" className="btn from-left">L4<br></br>単語</button></Link>
-                    <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="L4-kaku" className="btn from-left">L4<br></br>書き漢字</button></Link>
+                    <Link to={`/kanji?set=${kanjiSetClick}`}><button onClick={this.clickedKanjiSet} value="L4-kaku" className="btn from-left">L4<br></br>書き漢字</button></Link>
                     <Link to={`/kanji?set=${kanjiSetClick}`}><button disabled onClick={this.clickedKanjiSet} value="L4-yomu" className="btn from-right">L4<br></br>英語</button></Link>
                 </div>
 
